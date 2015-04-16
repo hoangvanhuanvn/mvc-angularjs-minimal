2d2e0d7989af598a4e5937c7282f8d643fde8754
Huan HOANG <huanhvhd@gmail.com>
2015-04-16 22:20:44 +0700

    init

---
+++ mvc4-angularjs-minimal/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace mvc4_angularjs_minimal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
