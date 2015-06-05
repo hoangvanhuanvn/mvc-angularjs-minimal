d66c3e6a3b1efce391e93602d13c18fd5a9a9d9e
Huan HOANG <huanhvhd@gmail.com>
2015-06-05 18:01:57 +0700

    Rename

---
+++ AngularjsApp.MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AngularjsApp.MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
